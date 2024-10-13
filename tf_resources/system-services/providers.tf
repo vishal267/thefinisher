961511152d061556a94c27481593746390ac8853
Vishal Arora <v.arora@unlimit.com>
2024-10-13 22:13:05 +0530

    argo app for system services

--- tf_resources/system-services/providers.tf
+++ tf_resources/system-services/providers.tf
@@ -1,10 +1,22 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
 provider "kubernetes" {
-  config_path = "~/.kube/config"  # Or specify an environment variable if you don't want to hardcode it.
+  config_path = "~/.kube/config" # Or specify an environment variable if you don't want to hardcode it.
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"  # Path to your kubeconfig file.
+    config_path = "~/.kube/config" # Path to your kubeconfig file.
   }
 }
+
+
 
